1e9ae71816527c6889527b863117596a438851d7
Shovon Basak <shvnbasak@gmail.com>
2023-12-13 03:49:30 +0600

    Tailwind is configured with theme css

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="text-3xl font-bold text-slate-300">Stack Overflow Next.js 14</div>
+    <div className="flex-center h-screen flex-col">
+      <h1 className="h1-bold">Stack Overflow Next.js 14 - H1 Bold</h1>
+      <h2 className="h2-bold">Stack Overflow Next.js 14 - H2 Bold</h2>
+      <h3 className="h3-bold">Stack Overflow Next.js 14 - H3 Bold</h3>
     </div>
   );
 }
