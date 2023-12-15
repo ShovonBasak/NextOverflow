6810e178989f33e204e3c2ceca861edcb6fbde92
Shovon Basak <shvnbasak@gmail.com>
2023-12-15 17:41:46 +0600

    Font, Meta and static assets are added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,35 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import './globals.css'
-import React from 'react'
- 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+import "./globals.css";
+import React from "react";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter, Space_Grotesk } from "next/font/google";
+import { Metadata } from "next";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metaData: Metadata = {
+  title: "NextFlow",
+  description: "A community driven platform for asking and answering programming questions. Get help share knowledge, and collaborate with developers around the world. Explore topics in web development, mobile app development, algorithms, data structure, and more.",
+  icons: {
+    icon: "@/public/images/site-logo.svg",
+  },
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>{children}</body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
