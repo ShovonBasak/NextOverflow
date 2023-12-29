b8d9867c0d22db09ad25e090c084707282624887
Shovon Basak <shvnbasak@gmail.com>
2023-12-29 23:38:55 +0600

    Fixed issues with search bar

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -1,11 +1,13 @@
-import Image from 'next/image'
-import { Input } from "@/components/ui/input"
+"use client";
+
+import Image from "next/image";
+import { Input } from "@/components/ui/input";
 
 const GlobalSearch = () => {
   return (
     <div className="relative w-full max-w-[600px] max-lg:hidden">
       <div className="background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4">
-        <Image 
+        <Image
           src="/assets/icons/search.svg"
           alt="search"
           width={24}
@@ -17,11 +19,12 @@ const GlobalSearch = () => {
           type="text"
           placeholder="Search globally"
           value=""
+          onChange={() => {}}
           className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default GlobalSearch
+export default GlobalSearch;
