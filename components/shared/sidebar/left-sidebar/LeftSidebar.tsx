cb9e0fa7bc0e08b8aa76ed59af96006d1580735d
Shovon Basak <shvnbasak@gmail.com>
2023-12-29 23:38:16 +0600

    Class name ordering is fixed by tailwind class name ordering extension

--- components/shared/sidebar/left-sidebar/LeftSidebar.tsx
+++ components/shared/sidebar/left-sidebar/LeftSidebar.tsx
@@ -3,7 +3,7 @@ import AuthGroup from "./auth-group/AuthGroup";
 
 const LeftSidebar = () => {
   return (
-    <section className="background-light900_dark200 custom-scrollbar light-border sticky left-0 top-0 flex  h-screen flex-col justify-between overflow-y-auto px-4 pt-36 max-sm:hidden">
+    <section className="background-light900_dark200 custom-scrollbar light-border sticky left-0 top-0 flex h-screen  flex-col justify-between overflow-y-auto border-r px-4 pt-36 max-sm:hidden">
       <MenuGroup />
       <AuthGroup />
     </section>
