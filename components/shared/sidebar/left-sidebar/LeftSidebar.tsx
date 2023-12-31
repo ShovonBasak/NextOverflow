df86e9bf0538f8a13ee47ec265ebca0412b5dc84
Shovon Basak <shvnbasak@gmail.com>
2023-12-31 06:09:42 +0600

    Updated css class for left sidebar

--- components/shared/sidebar/left-sidebar/LeftSidebar.tsx
+++ components/shared/sidebar/left-sidebar/LeftSidebar.tsx
@@ -3,7 +3,7 @@ import AuthGroup from "./auth-group/AuthGroup";
 
 const LeftSidebar = () => {
   return (
-    <section className="background-light900_dark200 custom-scrollbar light-border sticky left-0 top-0 flex h-screen  flex-col justify-between overflow-y-auto border-r px-4 pt-36 max-sm:hidden">
+    <section className="background-light900_dark200 light-border custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
       <MenuGroup />
       <AuthGroup />
     </section>
