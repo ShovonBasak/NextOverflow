a2f8124141d1a71b9fc04b2e0e2d191da3fb76e2
Shovon Basak <shvnbasak@gmail.com>
2024-01-01 23:53:40 +0600

    Updated css classes for filter

--- components/shared/Filter.tsx
+++ components/shared/Filter.tsx
@@ -26,10 +26,12 @@ const Filter = () => {
         ))}
       </ToggleGroup>
 
-      <div className="sm:hidden">
+      <div className="relative hidden max-md:flex">
         <Select>
-          <SelectTrigger className="body-regular light-border background-light800_dark300 text-dark500_light700 border px-5 py-2.5">
-            <SelectValue placeholder="Select a Filter" />
+          <SelectTrigger className="body-regular light-border background-light800_dark300 text-dark500_light700 min-h-[56px] border px-5 py-2.5 sm:min-w-[170px]">
+            <div className="line-clamp-1 flex-1 text-left">
+              <SelectValue placeholder="Select a Filter" />
+            </div>
           </SelectTrigger>
           <SelectContent className="text-dark500_light700 small-regular border-none bg-light-900 dark:bg-dark-300">
             {HomePageFilters.map((filter) => {
