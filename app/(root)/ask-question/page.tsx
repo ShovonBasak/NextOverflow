b062effbb6c4ce2475eb302cae382fa91adc1484
Shovon Basak <shvnbasak@gmail.com>
2024-01-22 00:50:28 +0600

    Dummy user data passed to the Question to use that as author object

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,10 +1,23 @@
 import Question from "@/components/form/Question";
+import { getUserByClerkId } from "@/lib/actions/user.action";
+import { useAuth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const AskQuestion = () => {
+const AskQuestion = async () => {
+  // const { clerkId } = useAuth();
+
+  const clerkId = "123456789";
+
+  if (!clerkId) {
+    redirect("/sign-in");
+  }
+
+  const mongoUser = await getUserByClerkId(clerkId);
+
   return (
     <div className="">
       <h1 className="h1-bold">Ask a question</h1>
-      <Question />
+      <Question userId={JSON.stringify(mongoUser._id)} />
     </div>
   );
 };
