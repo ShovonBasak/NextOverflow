f6b4a9297bdffd4e9557f2e6accb2ffeeb84515b
Shovon Basak <shvnbasak@gmail.com>
2024-01-28 02:24:14 +0600

    Added Question details page

---
+++ app/(root)/questions/[id]/page.tsx
@@ -0,0 +1,71 @@
+import Metric from "@/components/shared/Metric";
+import Tag from "@/components/shared/Tag";
+import { getQuestionById } from "@/lib/actions/question.action";
+import { formatAndDivideNumber, getTimestamp } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+
+const QuestionDetail = async ({ params }: { params: { id: string } }) => {
+  const question = await getQuestionById({ questionId: params.id });
+
+  return (
+    <>
+      <div className="flex-start w-full flex-col">
+        <div className="flex w-full flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
+          <Link
+            href={`/profile/${question.author.clerkId}`}
+            className="flex items-center justify-start gap-1"
+          >
+            <Image
+              src={question.author.image}
+              className="rounded-full"
+              width={22}
+              height={22}
+              alt="profile"
+            />
+            <p className="paragraph-semibold text-dark300_light700">{question.author.name}</p>
+          </Link>
+          <div className="flex justify-end">VOTING</div>
+        </div>
+        <h2 className="h2-semibold text-dark200_light900 mt-3.5 w-full text-left">{question.title}</h2>
+      </div>
+
+      <div className="mb-8 mt-5 flex flex-wrap gap-4">
+        <Metric
+          imgUrl="/assets/icons/clock.svg"
+          alt="clock icon"
+          value={` asked ${getTimestamp(question.createdAt)}`}
+          title=" Asked"
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/assets/icons/message.svg"
+          alt="message"
+          value={formatAndDivideNumber(question.answers.length)}
+          title=" Answers"
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/assets/icons/eye.svg"
+          alt="eye"
+          value={formatAndDivideNumber(question.views)}
+          title=" Views"
+          textStyles="small-medium text-dark400_light800"
+        />
+      </div>
+
+      <div className="mt-8 flex flex-wrap gap-2">
+        {question.tags.map((tag: any) => (
+          <Tag
+            key={tag._id}
+            _id={tag._id}
+            name={tag.name}
+            showCount={false}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default QuestionDetail;
