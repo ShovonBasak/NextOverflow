98b47b7924b7fffd0154a090fad1be8bebfc089b
Shovon Basak <shvnbasak@gmail.com>
2024-01-30 01:11:05 +0600

    "chevron right icon" loading issue is fixed

--- components/shared/sidebar/right-sidebar/top-questions/TopQuestion.tsx
+++ components/shared/sidebar/right-sidebar/top-questions/TopQuestion.tsx
@@ -13,15 +13,13 @@ const TopQuestion = () => {
             href={`/questions/${question.id}`}
             className="flex cursor-pointer items-center justify-between gap-7"
           >
-            <p className="body-medium text-dark500_light700">
-              {question.title}
-            </p>
+            <p className="body-medium text-dark500_light700">{question.title}</p>
             <Image
-              src="assets/icons/chevron-right.svg"
+              src="/assets/icons/chevron-right.svg"
               width={20}
               height={20}
               alt="chevron right icon"
-              className="invert-colors"
+              className="invert-colors h-[20px] w-[20px]"
             />
           </Link>
         ))}
